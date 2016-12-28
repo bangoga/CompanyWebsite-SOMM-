4d17b1a8830952723534ec10b19e774c267706fe
hawschiat <haw1997@hotmail.com>
2016-12-28 01:49:06 -0500

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -3,25 +3,33 @@ $(document).ready(function() {
 	var brand = $(".navbar-brand img");
 	var navicons = $(".navicons");
 	   $(window).scroll(function(){
+		   if($(window).width()>=992){
 		   if ($(document).scrollTop()<20){
-				   navbar.height('7.5%');
+				   navbar.height('10.5%');
 				   brand.stop().animate({
 					   height:'80px',
 					   width:'160px',
-					   marginLeft:'0'
+					   marginTop:'0%'
 					},30,"linear");
 					navicons.stop().animate({
-						marginTop:"1.3%"
+						marginTop:"0.7%"
 					},30,"linear");
 		   } else {
-				   navbar.height('7%');
+				   navbar.height('8.5%');
 				   brand.stop().animate({
 					   height:'50px',
 					   width:'90px',
-					   marginLeft:'1vw'
+					   marginTop:'10%'
 					},30,"linear");
 					navicons.stop().animate({
-						marginTop:"0.3%"
+						marginTop:"0%"
+					},30,"linear");
+		   }} else {
+				   navbar.height('10.5%');
+				   brand.stop().animate({
+					   height:'80px',
+					   width:'160px',
+					   marginTop:'0%'
 					},30,"linear");
 		   }
 	   });
